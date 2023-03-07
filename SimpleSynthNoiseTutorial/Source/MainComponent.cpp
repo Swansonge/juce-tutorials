2e239d1a36eb2ef69903804d8be871e04de00bf7
Eric Swanson <51669372+swansonbroth96@users.noreply.github.com>
2023-03-06 17:11:28 -0800

    change gains sliders to use decibels

--- SimpleSynthNoiseTutorial/Source/MainComponent.cpp
+++ SimpleSynthNoiseTutorial/Source/MainComponent.cpp
@@ -1,5 +1,6 @@
 #include "MainComponent.h"
 
+
 //==============================================================================
 MainComponent::MainComponent()
 {
@@ -9,6 +10,30 @@ MainComponent::MainComponent()
 
     // Specify the number of input and output channels that we want to open
     setAudioChannels (0, 2);
+
+    //set range in decibels
+    leftDecibelSlider.setRange(-96.0f, 0.0f);
+    rightDecibelSlider.setRange(-96.0f, 0.0f);
+    leftLinearSlider.setRange(0.0f, 1.0f);
+    rightLinearSlider.setRange(0.0f, 1.0f);
+
+    leftDecibelSlider.setValue(juce::Decibels::gainToDecibels(leftLevel, -96.0f));
+    rightDecibelSlider.setValue(juce::Decibels::gainToDecibels(rightLevel, -96.0f));
+    leftLinearSlider.setValue(0.0f);
+    rightLinearSlider.setValue(0.0f);
+
+    //lambda functions that will be called whenever slider value is changed
+    //1) convert value of gain to decibels 
+    leftDecibelSlider.onValueChange = [this] { leftLevel = juce::Decibels::decibelsToGain((float)leftDecibelSlider.getValue(), -96.0f); };
+    rightDecibelSlider.onValueChange = [this] { rightLevel = juce::Decibels::decibelsToGain((float)rightDecibelSlider.getValue(), -96.0f); };
+    //leftLinearSlider.onValueChange = [this] { leftLevel = leftLinearSlider.getValue(); leftDecibelSlider.setValue(juce::Decibels::gainToDecibels((float)leftLevel)); };
+    //rightLinearSlider.onValueChange = [this] { rightLevel = rightLinearSlider.getValue(); rightDecibelSlider.setValue(juce::Decibels::gainToDecibels((float)rightLevel)); };
+
+    leftDecibelLabel.setText("Noise Level in dB", juce::dontSendNotification);
+    rightDecibelLabel.setText("Noise Level in dB", juce::dontSendNotification);
+    leftLinearLabel.setText("Noise Level (linear)", juce::dontSendNotification);
+    rightLinearLabel.setText("Noise Level (linear)", juce::dontSendNotification);
+
 }
 
 MainComponent::~MainComponent()
@@ -39,9 +64,12 @@ void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& buffe
 
     for (auto channel = 0; channel < bufferToFill.buffer->getNumChannels(); channel++)
     {
+        //get function-local copy of level
+        auto currentLeftLevel = leftLevel;
+        auto leftLevelScale = currentLeftLevel * 2.0f;
 
-        //Get a pointer to the start sample in the buffer for this audio output channel
-        auto* buffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
+        auto currentRightLevel = rightLevel;
+        auto rightLevelScale = currentRightLevel * 2.0f;
 
         //Fill the required number of samples with noise between -0.125 and +0.125
         for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
@@ -49,19 +77,19 @@ void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& buffe
             //adjust individual levels for left and right sliders
             if (channel == 0) 
             {
-                auto leftLevel = (float)leftSlider.getValue();
-                auto leftLevelScale = leftLevel * 2.0f;
+                //Get a pointer to the start sample in the buffer for this audio output channel
+                auto* buffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
 
                 //generate white noise.To get range of -x to x for noise, multiply nextFloat() * 2x then subtract x
-                buffer[sample] = random.nextFloat() * leftLevelScale - leftLevel;
+                buffer[sample] = random.nextFloat() * leftLevelScale - currentLeftLevel;
             }
             else if (channel == 1)
             {
-                auto rightLevel = (float)rightSlider.getValue();
-                auto rightLevelScale = rightLevel * 2.0f;
 
-                //generate white noise.To get range of -x to x for noise, multiply nextFloat() * 2x then subtract x
-                buffer[sample] = random.nextFloat() * rightLevelScale - rightLevel;
+                //Get a pointer to the start sample in the buffer for this audio output channel
+                auto* buffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
+
+                buffer[sample] = random.nextFloat() * rightLevelScale - currentRightLevel;
             }
             
             
@@ -83,25 +111,37 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    //left channel slider
-    addAndMakeVisible(leftSlider);
-    leftSlider.setRange(0.0f, 1.0f);
-    leftSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxRight, false, 75, 25);
+    //left channel decibel slider
+    addAndMakeVisible(leftDecibelSlider);
+    leftDecibelSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxRight, false, 75, 25);
+
+    ////left channel linear slider
+    //addAndMakeVisible(leftLinearSlider);
+    //leftLinearSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxRight, false, 75, 25);
     
-    //left slider label
-    addAndMakeVisible(leftLevelLabel);
-    leftLevelLabel.setText("Noise Level", juce::dontSendNotification);
-    leftLevelLabel.attachToComponent(&leftSlider, true);
+    //left slider decibel label
+    addAndMakeVisible(leftDecibelLabel);
+    leftDecibelLabel.attachToComponent(&leftDecibelSlider, true);
 
-    //right channel slider
-    addAndMakeVisible(rightSlider);
-    rightSlider.setRange(0.0f, 1.0f);
-    rightSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxRight, false, 75, 25);
+    ////left slider linear label
+    //addAndMakeVisible(leftLinearLabel);
+    //leftLinearLabel.attachToComponent(&leftLinearSlider, true);
 
-    //right slider label
-    addAndMakeVisible(rightLevelLabel);
-    rightLevelLabel.setText("Noise Level", juce::dontSendNotification);
-    rightLevelLabel.attachToComponent(&rightSlider, true);
+    //right channel decibel slider
+    addAndMakeVisible(rightDecibelSlider);
+    rightDecibelSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxRight, false, 75, 25);
+
+    ////right channel linear slider
+    //addAndMakeVisible(rightLinearSlider);
+    //rightLinearSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxRight, false, 75, 25);
+
+    //right slider decibel label
+    addAndMakeVisible(rightDecibelLabel);
+    rightDecibelLabel.attachToComponent(&rightDecibelSlider, true);
+
+    ////right slider linear label
+    //addAndMakeVisible(rightLinearLabel);
+    //rightLinearLabel.attachToComponent(&rightLinearSlider, true);
 
 }
 
@@ -112,7 +152,9 @@ void MainComponent::resized()
     // update their positions.
 
     auto sliderLeft = 120;
-    leftSlider.setBounds(sliderLeft, 50, getWidth() - sliderLeft * 2, 50);
-    rightSlider.setBounds(sliderLeft, 150, getWidth() - sliderLeft * 2, 50);
+    leftDecibelSlider.setBounds(sliderLeft, 50, getWidth() - sliderLeft * 2, 50);
+    rightDecibelSlider.setBounds(sliderLeft, 100, getWidth() - sliderLeft * 2, 50); 
+    leftLinearSlider.setBounds(sliderLeft, 150, getWidth() - sliderLeft * 2, 50);
+    rightLinearSlider.setBounds(sliderLeft, 200, getWidth() - sliderLeft * 2, 50);
     
 }
--- SimpleSynthNoiseTutorial/Source/MainComponent.h
+++ SimpleSynthNoiseTutorial/Source/MainComponent.h
@@ -2,6 +2,35 @@
 
 #include <JuceHeader.h>
 
+
+//=========== custom decibel slider class =============
+class DecibelSlider : public juce::Slider
+{
+public:
+    DecibelSlider() {}
+
+    double getValueFromText(const juce::String& text) override
+    {
+        //usually -100 is default -INF. Changing to -90.0 as part of tutorial exercise
+        auto minusInfinitydB = -96.0;
+
+        auto decibelText = text.upToFirstOccurrenceOf("dB", false, false).trim(); 
+
+        //check if text = -INF. IF so, return -96 ( custom value of -inf as defined in this method). Else, return number value from text
+        return decibelText.equalsIgnoreCase("-INF") ? minusInfinitydB
+            : decibelText.getDoubleValue(); 
+    }
+
+    juce::String getTextFromValue(double value) override
+    {
+        //change default -INF from -100 to -96
+        return juce::Decibels::toString(value, 2, -96.0);
+    }
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(DecibelSlider)
+};
+
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
@@ -26,11 +55,17 @@ public:
 private:
     //==============================================================================
 
-    juce::Slider leftSlider;
-    juce::Slider rightSlider;
-    juce::Label leftLevelLabel;
-    juce::Label rightLevelLabel;
+    DecibelSlider leftDecibelSlider;
+    DecibelSlider rightDecibelSlider;
+    juce::Slider leftLinearSlider;
+    juce::Slider rightLinearSlider;
+    juce::Label leftDecibelLabel;
+    juce::Label rightDecibelLabel;
+    juce::Label leftLinearLabel;
+    juce::Label rightLinearLabel;
     juce::Random random;
+    float leftLevel{ 0.0f};
+    float rightLevel{ 0.0f };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
